eac0e3400178aa057f0eded5dad94ce33131efa2
jose-aluno <josealuno70@gmail.com>
2025-06-07 21:16:53 +0000

    feat: criação do repositório de Emprestimo

---
+++ src/repository/EmprestimoRepository.ts
@@ -0,0 +1,42 @@
+import { EmprestimoEntity } from "../model/EmprestimoEntity"
+
+export class EmprestimoRepository {
+    private static instance: EmprestimoRepository
+    private emprestimoList: EmprestimoEntity[] = []
+
+    private constructor() {}
+
+    public static getInstance(): EmprestimoRepository {
+        if (!this.instance) {
+            this.instance = new EmprestimoRepository()
+        }
+        return this.instance
+    }
+
+    inserir(emprestimo: EmprestimoEntity) {
+        this.emprestimoList.push(emprestimo)
+    }
+
+    listar(): EmprestimoEntity[] {
+        return this.emprestimoList
+    }
+
+    buscarPorId(id: number): EmprestimoEntity | undefined {
+        return this.emprestimoList.find(e => e.id === id)
+    }
+
+    atualizar(id: number, usuario_id: number, estoque_id: number, data_emprestimo: Date, data_devolucao: Date, data_entrega: Date, dias_atraso: number, suspensao_ate: Date): boolean {
+        const emprestimo = this.buscarPorId(id)
+        if (!emprestimo) return false
+
+        emprestimo.usuario_id = usuario_id
+        emprestimo.estoque_id = estoque_id
+        emprestimo.data_emprestimo = data_emprestimo
+        emprestimo.data_devolucao = data_devolucao
+        emprestimo.data_entrega = data_entrega
+        emprestimo.dias_atraso = dias_atraso
+        emprestimo.suspensao_ate = suspensao_ate
+
+        return true
+    }
+}
